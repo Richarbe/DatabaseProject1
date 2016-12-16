cb378fc5d2ce9c9753e6829054a2cdf26347dff4
Richarbe <benr239@gmail.com>
2016-12-16 12:49:26 -0800

    New view

--- RocketQueries2.sql
+++ RocketQueries2.sql
@@ -5,6 +5,17 @@ select organization_id, org_name, date_format(start_date,'%Y-%m-%d') as start_da
 from organization o
 left join location l on l.location_id = o.location_id;
 
-drop view organizationfull;
-
 select * from organization_full;
+
+#launch_pad view
+create or replace view launch_pad_full as
+select launch_pad_id, pad_name, lp.organization_id, org_name, lp.location_id, loc_name, address
+from launch_pad lp
+left join organization o on o.organization_id = lp.organization_id
+left join location l on l.location_id = lp.location_id;
+
+select * from launch_pad_full;
+
+select * from organization;
+
+INSERT INTO organization (org_name, start_date, end_date, location_id) VALUES ('asdflaskdf',0,0,NULL)
